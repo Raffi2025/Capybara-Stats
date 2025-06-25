29e2b83c0d2793010785485e57ee1ecd1466b515
Raffi2025 <raffimasters@icloud.com>
2025-06-25 02:51:03 +0100

    Update Pitchers.py

--- Pitchers.py
+++ Pitchers.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv("Pitchers.csv")
+data = pd.read_csv("pitchers.csv")
 st.subheader(f"Pitchers ({len(data)} Entries):")
 st.write(data)
