1f083e90d1a5e49517089f92a5922665a78ab537
Raffi2025 <raffimasters@icloud.com>
2025-06-26 14:15:00 +0100

    Update Batters.py

--- Batters.py
+++ Batters.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import MainApp
 
+#Full table
 data = pd.read_csv("batters.csv")
 st.subheader(f"Batters")
 st.write(data)
+
+#Percentile Graph
+st.subheader("Percentile Graph")
+PlateAppearances(batterList)
